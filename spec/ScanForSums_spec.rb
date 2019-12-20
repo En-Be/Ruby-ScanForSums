9bac826efb0463873fe7e98f910b4174a034ba31
Nicholas <n3ttl3t@gmail.com>
2019-12-20 13:27:30 +0000

    doesn't recognise multiplication

--- spec/ScanForSums_spec.rb
+++ spec/ScanForSums_spec.rb
@@ -1,5 +1,10 @@
 describe 'ScanForSums' do
+
     it 'Returns 1 + 1 as 2' do
         expect(ScanForSums("1 + 1")).to eq ["1 + 1", 2]
+    end
+
+    it 'Returns 1 * 1 as 1' do
+        expect(ScanForSums("1 * 1")).to eq ["1 * 1", 1]
     end
 end
