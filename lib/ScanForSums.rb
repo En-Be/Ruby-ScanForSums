29736664cdef714c8be3d39c74547e19a083b2c1
Nicholas <n3ttl3t@gmail.com>
2019-12-20 14:24:12 +0000

    refactored

--- lib/ScanForSums.rb
+++ lib/ScanForSums.rb
@@ -1,26 +1,36 @@
+# def ScanForSums(arg)
+#     result = [arg]
+
+#     equation = arg.split(' ')
+
+#     if equation[0].to_i.to_s != equation[0] || equation[0].to_i.to_s != equation[0]
+#         raise("Input must be a valid mathematical string")
+#     end
+
+#     if equation[1] == '+'
+#         result.push(equation[0].to_f + equation[2].to_f)
+#     elsif equation[1] == '*'
+#         result.push(equation[0].to_f * equation[2].to_f)
+#     elsif equation[1] == '-'
+#         result.push(equation[0].to_f - equation[2].to_f)
+#     elsif equation[1] == '/'
+#         result.push(equation[0].to_f / equation[2].to_f)
+#     end
+
+#     result
+# end
+
+# --- REFACTOR 
+
 def ScanForSums(arg)
     result = [arg]
-
     equation = arg.split(' ')
 
     if equation[0].to_i.to_s != equation[0] || equation[0].to_i.to_s != equation[0]
         raise("Input must be a valid mathematical string")
     end
 
-    if equation[1] == '+'
-        result.push(equation[0].to_f + equation[2].to_f)
-    elsif equation[1] == '*'
-        result.push(equation[0].to_f * equation[2].to_f)
-    elsif equation[1] == '-'
-        result.push(equation[0].to_f - equation[2].to_f)
-    elsif equation[1] == '/'
-        result.push(equation[0].to_f / equation[2].to_f)
-    end
-
+    result.push(equation[0].to_f.public_send(equation[1].to_sym,equation[2].to_f))
     result
 end
-
-
-
-
 
