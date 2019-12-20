4fc68a61f416884bae504a2d23621449ed67fd93
Nicholas <n3ttl3t@gmail.com>
2019-12-20 13:59:21 +0000

    recognises addition

--- lib/ScanForSums.rb
+++ lib/ScanForSums.rb
@@ -1,3 +1,23 @@
 def ScanForSums(arg)
-    ["1 + 1", 2]
+    result = [arg]
+
+    equation = arg.split(' ')
+    if equation[1] == '+'
+        result.push(equation[0].to_i + equation[2].to_i)
+    end
+
+    result
 end
+
+
+
+
+
+# def ScanForSums(arg)
+#     result = []
+#     equation = arg.split(' ')
+#     # print("#{equation[0].to_i} #{equation[1].to_sym} #{equation[2].to_i}")
+#     sum = equation[0].to_i, equation[1].to_sym, equation[2].to_i
+#     result.push(sum[0].public_send(sum[1],sum[2]))
+#     result
+# end
--- spec/ScanForSums_spec.rb
+++ spec/ScanForSums_spec.rb
@@ -7,4 +7,8 @@ describe 'ScanForSums' do
     it 'Returns 1 * 1 as 1' do
         expect(ScanForSums("1 * 1")).to eq ["1 * 1", 1]
     end
+
+    it 'Finds the operator symbol' do
+
+    end
 end
