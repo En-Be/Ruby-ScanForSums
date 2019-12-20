5e0bbc1453e4f44d940c347732b43933f99a30fa
Nicholas <n3ttl3t@gmail.com>
2019-12-20 14:03:31 +0000

    recognises division

--- spec/ScanForSums_spec.rb
+++ spec/ScanForSums_spec.rb
@@ -12,6 +12,10 @@ describe 'ScanForSums' do
         expect(ScanForSums("1 - 1")).to eq ["1 - 1", 0]
     end
 
+    it 'Returns 1 / 1 as 1' do
+        expect(ScanForSums("1 / 1")).to eq ["1 / 1", 1]
+    end
+
     it 'Finds the operator symbol' do
         # expect
     end
