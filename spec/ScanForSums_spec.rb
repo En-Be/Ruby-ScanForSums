e5ffa6f735aef9bbc65a9c09016e5d6e043a8694
Nicholas <n3ttl3t@gmail.com>
2019-12-20 14:01:34 +0000

    does not recognise subtraction

--- spec/ScanForSums_spec.rb
+++ spec/ScanForSums_spec.rb
@@ -8,7 +8,11 @@ describe 'ScanForSums' do
         expect(ScanForSums("1 * 1")).to eq ["1 * 1", 1]
     end
 
-    it 'Finds the operator symbol' do
+    it 'Returns 1 - 1 as 0' do
+        expect(ScanForSums("1 - 1")).to eq ["1 - 1", 0]
+    end
 
+    it 'Finds the operator symbol' do
+        # expect
     end
 end
